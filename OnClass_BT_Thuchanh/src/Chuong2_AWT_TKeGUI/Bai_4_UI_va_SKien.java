2c8890a06256ffbb9a477f088d7c8a7f6a29b6cc
Phạm Văn Quyết <quyet05122002@gmail.com>
2023-09-05 17:31:09 +0700

    Đang làm dở bài 5 | 5:30 p.m | 9/5/2023

--- OnClass_BT_Thuchanh/src/Chuong2_AWT_TKeGUI/Bai_4_UI_va_SKien.java
+++ OnClass_BT_Thuchanh/src/Chuong2_AWT_TKeGUI/Bai_4_UI_va_SKien.java
@@ -5,10 +5,8 @@
  */
 package Chuong2_AWT_TKeGUI;
 
+import java.awt.BorderLayout;
 import java.awt.Dimension;
-import java.awt.FlowLayout;
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -17,9 +15,9 @@ import java.awt.event.ItemListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import javax.swing.JButton;
-import javax.swing.JCheckBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
@@ -100,141 +98,146 @@ public class Bai_4_UI_va_SKien extends JFrame implements ActionListener, ItemLis
 //    }
 
     /*  */
-// /*Hình 3*/
-//    JTextField t1, t2;
-//    JButton b1, b2, b3, b4, b5;
-//    JLabel l1;
-//    JButton[] buttons;
-//
-//    @Override
-//    public void actionPerformed(ActionEvent e) {
-//        try {
-//            if (e.getSource() == b1) {
-//                l1.setText(Double.toString(Double.parseDouble(t1.getText()) + (Double.parseDouble(t2.getText()))));
-//            }
-//            if (e.getSource() == b2) {
-//                l1.setText(Double.toString(Double.parseDouble(t1.getText()) - (Double.parseDouble(t2.getText()))));
-//            }
-//            if (e.getSource() == b3) {
-//                l1.setText(Double.toString(Double.parseDouble(t1.getText()) * (Double.parseDouble(t2.getText()))));
-//            }
-//            if (e.getSource() == b4) {
-//                l1.setText(Double.toString(Double.parseDouble(t1.getText()) / (Double.parseDouble(t2.getText()))));
-//            }
-//            if (e.getSource() == b5) {
-//                int choice = JOptionPane.showConfirmDialog(this, "Bạn có muốn thoát chương trình?", "Xác nhận", JOptionPane.YES_NO_OPTION);
-//                if (choice == JOptionPane.YES_OPTION) {
-//                    System.exit(0); // Thoát chương trình
-//                }
-//            }
-//        } catch (Exception a) {
-//            l1.setText(a.getMessage().toString());
-//        }
-//    }
-//
-//    public Bai_4_UI_va_SKien() {
-//        super("Event demo! Tính toán.");
-//        this.setSize(400, 250);
-//        this.setLayout(new BorderLayout());
-//
-//        JPanel jPanel1 = new JPanel();
-//        jPanel1.setLayout(new GridLayout(3, 2));
-//        jPanel1.setPreferredSize(new Dimension(300, 100));
-//        this.add(jPanel1, BorderLayout.NORTH);
-//
-//        t1 = new JTextField();
-//        t2 = new JTextField();
-//        b1 = new JButton("Cong");
-//        b2 = new JButton("Tru");
-//        b3 = new JButton("Nhan");
-//        b4 = new JButton("Chia");
-//        b5 = new JButton("Thoat");
-//        l1 = new JLabel();
-//        buttons = new JButton[]{b1, b2, b3, b4, b5};
-//
-//        jPanel1.add(new JLabel("So thu nhat: "));
-//        jPanel1.add(t1);
-//        jPanel1.add(new JLabel("So thu hai: "));
-//        jPanel1.add(t2);
-//        jPanel1.add(new JLabel());
-//        jPanel1.add(l1);
-//
-//        JPanel jPanel2 = new JPanel();
-//        jPanel2.setLayout(new GridLayout(3, 2));
-//        this.add(jPanel2, BorderLayout.SOUTH);
-//        for (JButton button : buttons) {
-//            jPanel2.add(button);
-//            button.addActionListener(this);
-//        }
-//    }
-    /**/
- /*Hình 4*/
-    JTextField t1, t2, t3;
-    JCheckBox c1, c2;
-    JButton b1, b2, b3, b4;
-
-    @Override
-    public void actionPerformed(ActionEvent e) {
-
-    }
+ /*Hình 3*/
+    JTextField t1, t2;
+    JButton b1, b2, b3, b4, b5;
+    JLabel l1;
+    JButton[] buttons;
     
     @Override
     public void itemStateChanged(ItemEvent e) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        try {
+            if (e.getSource() == b1) {
+                l1.setText(Double.toString(Double.parseDouble(t1.getText()) + (Double.parseDouble(t2.getText()))));
+            }
+            if (e.getSource() == b2) {
+                l1.setText(Double.toString(Double.parseDouble(t1.getText()) - (Double.parseDouble(t2.getText()))));
+            }
+            if (e.getSource() == b3) {
+                l1.setText(Double.toString(Double.parseDouble(t1.getText()) * (Double.parseDouble(t2.getText()))));
+            }
+            if (e.getSource() == b4) {
+                l1.setText(Double.toString(Double.parseDouble(t1.getText()) / (Double.parseDouble(t2.getText()))));
+            }
+            if (e.getSource() == b5) {
+                int choice = JOptionPane.showConfirmDialog(this, "Bạn có muốn thoát chương trình?", "Xác nhận", JOptionPane.YES_NO_OPTION);
+                if (choice == JOptionPane.YES_OPTION) {
+                    System.exit(0); // Thoát chương trình
+                }
+            }
+        } catch (Exception a) {
+            l1.setText(a.getMessage().toString());
+        }
+    }
+
     public Bai_4_UI_va_SKien() {
-        super("Bai tap thuc hanh 1");
-        this.setLayout(new GridBagLayout());
-        GridBagConstraints gbc = new GridBagConstraints();
-        gbc.gridx = 0;
-        gbc.gridy = 0;
-        this.setSize(450, 250);
+        super("Event demo! Tính toán.");
+        this.setSize(400, 250);
+        this.setLayout(new BorderLayout());
 
         JPanel jPanel1 = new JPanel();
-        jPanel1.setLayout(new GridLayout(5, 2));
-        jPanel1.setPreferredSize(new Dimension(400, 150));
-        this.add(jPanel1, gbc);
+        jPanel1.setLayout(new GridLayout(3, 2));
+        jPanel1.setPreferredSize(new Dimension(300, 100));
+        this.add(jPanel1, BorderLayout.NORTH);
 
-        t1 = new JTextField("");
-        t2 = new JTextField("");
-        t3 = new JTextField("");
-        c1 = new JCheckBox("Male");
-        c2 = new JCheckBox("Female");
-        b1 = new JButton("Add");
-        b2 = new JButton("Find");
-        b3 = new JButton("Delete");
-        b4 = new JButton("Update");
-        JButton[] buttons = new JButton[]{b1, b2, b3, b4};
+        t1 = new JTextField();
+        t2 = new JTextField();
+        b1 = new JButton("Cong");
+        b2 = new JButton("Tru");
+        b3 = new JButton("Nhan");
+        b4 = new JButton("Chia");
+        b5 = new JButton("Thoat");
+        l1 = new JLabel();
+        buttons = new JButton[]{b1, b2, b3, b4, b5};
 
-        jPanel1.add(new JLabel("Name: "));
+        jPanel1.add(new JLabel("So thu nhat: "));
         jPanel1.add(t1);
-        jPanel1.add(new JLabel("Birth: "));
+        jPanel1.add(new JLabel("So thu hai: "));
         jPanel1.add(t2);
-        jPanel1.add(new JLabel("Address: "));
-        jPanel1.add(t3);
-        jPanel1.add(new JLabel("Sex: "));
         jPanel1.add(new JLabel());
-        jPanel1.add(c1);
-        jPanel1.add(c2);
+        jPanel1.add(l1);
 
         JPanel jPanel2 = new JPanel();
-        jPanel2.setLayout(new FlowLayout());
-        jPanel2.setAlignmentX(CENTER_ALIGNMENT);
-        gbc.gridy = 2;
-        this.add(jPanel2, gbc);
-
-        jPanel2.add(b1);
-        jPanel2.add(b2);
-        jPanel2.add(b3);
-        jPanel2.add(b4);
-
-        c1.addItemListener(this);
-        c2.addItemListener(this);
+        jPanel2.setLayout(new GridLayout(3, 2));
+        this.add(jPanel2, BorderLayout.SOUTH);
         for (JButton button : buttons) {
+            jPanel2.add(button);
             button.addActionListener(this);
         }
     }
+    /**/
+ /*Hình 4*/
+//    JTextField t1, t2, t3;
+//    JCheckBox c1, c2;
+//    JButton b1, b2, b3, b4;
+//
+//    @Override
+//    public void actionPerformed(ActionEvent e) {
+//
+//    }
+//    
+//    @Override
+//    public void itemStateChanged(ItemEvent e) {
+//        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+//    }
+//
+//    public Bai_4_UI_va_SKien() {
+//        super("Bai tap thuc hanh 1");
+//        this.setLayout(new GridBagLayout());
+//        GridBagConstraints gbc = new GridBagConstraints();
+//        gbc.gridx = 0;
+//        gbc.gridy = 0;
+//        this.setSize(450, 250);
+//
+//        JPanel jPanel1 = new JPanel();
+//        jPanel1.setLayout(new GridLayout(5, 2));
+//        jPanel1.setPreferredSize(new Dimension(400, 150));
+//        this.add(jPanel1, gbc);
+//
+//        t1 = new JTextField("");
+//        t2 = new JTextField("");
+//        t3 = new JTextField("");
+//        c1 = new JCheckBox("Male");
+//        c2 = new JCheckBox("Female");
+//        b1 = new JButton("Add");
+//        b2 = new JButton("Find");
+//        b3 = new JButton("Delete");
+//        b4 = new JButton("Update");
+//        JButton[] buttons = new JButton[]{b1, b2, b3, b4};
+//
+//        jPanel1.add(new JLabel("Name: "));
+//        jPanel1.add(t1);
+//        jPanel1.add(new JLabel("Birth: "));
+//        jPanel1.add(t2);
+//        jPanel1.add(new JLabel("Address: "));
+//        jPanel1.add(t3);
+//        jPanel1.add(new JLabel("Sex: "));
+//        jPanel1.add(new JLabel());
+//        jPanel1.add(c1);
+//        jPanel1.add(c2);
+//
+//        JPanel jPanel2 = new JPanel();
+//        jPanel2.setLayout(new FlowLayout());
+//        jPanel2.setAlignmentX(CENTER_ALIGNMENT);
+//        gbc.gridy = 2;
+//        this.add(jPanel2, gbc);
+//
+//        jPanel2.add(b1);
+//        jPanel2.add(b2);
+//        jPanel2.add(b3);
+//        jPanel2.add(b4);
+//
+//        c1.addItemListener(this);
+//        c2.addItemListener(this);
+//        for (JButton button : buttons) {
+//            button.addActionListener(this);
+//        }
+//    }
 
     public static void main(String[] args) {
         Bai_4_UI_va_SKien bai_4_UI_va_SKien = new Bai_4_UI_va_SKien();
@@ -249,6 +252,8 @@ public class Bai_4_UI_va_SKien extends JFrame implements ActionListener, ItemLis
             }
         });
     }
+
+    
 
     
 
--- OnClass_BT_Thuchanh/src/Chuong2_AWT_TKeGUI/Bai_5_Caclulator.java
+++ OnClass_BT_Thuchanh/src/Chuong2_AWT_TKeGUI/Bai_5_Caclulator.java
@@ -5,10 +5,98 @@
  */
 package Chuong2_AWT_TKeGUI;
 
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
 /**
  *
  * @author quyet
  */
-public class Bai_5_Caclulator {
-    
+public class Bai_5_Caclulator extends JFrame {
+
+    JTextField t1;
+    JButton b_backSpc, b_C, b_CE, b_MC, b_MR, b_MS, b_M_, b_cong, b_tru, b_0, b_1, b_2, b_3, b_4, b_5, b_6, b_7, b_8, b_9, b_doi_dau, b_cham, b_phan_so, b_nhan, b_chia, b_phan_tram, b_sprt, b_bang;
+    JButton[] buttons1, buttons2, buttons3;
+
+    public Bai_5_Caclulator() {
+        super("Calculator - Java Nâng cao");
+        this.setLayout(new GridBagLayout());
+        this.setSize(600, 300);
+//        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setLocationRelativeTo(null);
+
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+
+        t1 = new JTextField("0");
+        t1.setPreferredSize(new Dimension(100, 50));
+        t1.setBackground(Color.blue);
+        t1.setForeground(Color.WHITE); // Đặt màu chữ thành trắng
+        t1.setHorizontalAlignment(JTextField.LEFT); // Căn lề văn bản sang trái
+        t1.setEditable(false);
+        b_backSpc = new JButton("Backspc");
+        b_C = new JButton("C");
+        b_CE = new JButton("CE");
+        b_MC = new JButton("MC");
+        b_MR = new JButton("MR");
+        b_MS = new JButton("MS");
+        b_M_ = new JButton("M+");
+        b_0 = new JButton("0");
+        b_1 = new JButton("1");
+        b_2 = new JButton("2");
+        b_3 = new JButton("3");
+        b_4 = new JButton("4");
+        b_5 = new JButton("5");
+        b_6 = new JButton("6");
+        b_7 = new JButton("7");
+        b_8 = new JButton("8");
+        b_9 = new JButton("9");
+        b_doi_dau = new JButton("+/-");
+        b_cham = new JButton(".");
+        b_cong = new JButton("+");
+        b_tru = new JButton("-");
+        b_nhan = new JButton("*");
+        b_chia = new JButton("/");
+        b_sprt = new JButton("sqrt");
+        b_phan_tram = new JButton("%");
+        b_phan_so = new JButton("1/X");
+        b_bang = new JButton("=");
+
+        buttons1 = new JButton[]{b_backSpc, b_C, b_CE};
+        buttons2 = new JButton[]{b_MC, b_7, b_8, b_9, b_MR, b_4, b_5, b_6, b_MS, b_1, b_2, b_3, b_M_, b_0, b_doi_dau, b_cham};
+        buttons3 = new JButton[]{b_chia, b_sprt, b_nhan, b_phan_tram, b_tru, b_phan_so, b_cong, b_bang};
+
+        // Thêm các thuộc tính khác của JTextField nếu cần
+        this.add(t1, gbc);
+
+        JPanel jPanel = new JPanel();
+        jPanel.setLayout(new FlowLayout((int) LEFT_ALIGNMENT));
+        t1.setForeground(Color.WHITE); // Đặt màu chữ thành trắng
+        for (JButton jButton : buttons1) {
+            jPanel.add(jButton);
+        }
+        this.add(jPanel);
+
+        this.addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        Bai_5_Caclulator bai_5_Caclulator = new Bai_5_Caclulator();
+        bai_5_Caclulator.setVisible(true);
+    }
+
 }
